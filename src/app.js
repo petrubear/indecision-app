f35c4948075dfcf5dc31b3778f8bce703447d921
petrubear <edison.martinez@gmail.com>
2021-07-10 15:32:47 -0500

    feat: random item selection

--- src/app.js
+++ src/app.js
@@ -22,9 +22,15 @@ const onFormSubmit = (e) => {
     }
 };
 
-const removeAll = () => {
+const onRemoveAll = () => {
     app.options = [];
     render();
+};
+
+const onMakeDecision = () => {
+    const randomNumber = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNumber];
+    alert(option);
 };
 
 const appRoot = document.getElementById('app');
@@ -35,8 +41,8 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
-            <button onClick={removeAll}>Remove All</button>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button disabled={app.options.length === 0} onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {app.options.map((option) => <li key={option}>{option}</li>)}
             </ol>
