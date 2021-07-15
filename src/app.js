85272d12a37a973d4973047988fa10b3a6e6ca07
petrubear <edison.martinez@gmail.com>
2021-07-15 18:48:24 -0500

    feat: add normalize.css

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
+import 'normalize.css/normalize.css';
 
 const appRoot = document.getElementById('app');
 ReactDOM.render(<IndecisionApp options={[]}/>, appRoot);
