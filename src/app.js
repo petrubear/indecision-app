40f208bc7ef56124c7b30d3dcc5e8e33a3b39b35
petrubear <edison.martinez@gmail.com>
2021-07-14 19:15:16 -0500

    feat: add scss support

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss';
 
 const appRoot = document.getElementById('app');
 ReactDOM.render(<IndecisionApp options={[]}/>, appRoot);
