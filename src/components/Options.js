29c9757666da291e5e85845301ad19a016c002d4
petrubear <edison.martinez@gmail.com>
2021-07-15 19:40:56 -0500

    feat: widget styles

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,12 +5,15 @@ import React from 'react';
 
 const Options = (props) => (
     <div>
-        <button
-            className="button button--link"
-            onClick={props.handleDeleteOptions}>
-            RemoveAll
-        </button>
-        {props.options.length === 0 && <p>Please add an option to get started.</p>}
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button
+                className="button button--link"
+                onClick={props.handleDeleteOptions}>
+                RemoveAll
+            </button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started.</p>}
         {props.options.map((option) => {
             return <Option
                 key={option}
